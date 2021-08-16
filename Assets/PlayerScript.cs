b9bce8b99b19d77600100477c82c6fcf41101b3d
GlaM4X <glam4x@mail.ru>
2021-08-16 22:58:09 +0300

    Zero version
    Мало мало работающая херня.
    Починил камеру, все подключается, синхронизируется позиция и вращение игроков, сбоит анимация

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+public class PlayerScript : NetworkBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.gameObject.GetComponent<CoverShooter.ThirdPersonCamera>().Target = gameObject.GetComponent<CoverShooter.CharacterMotor>();
+
+        Camera.main.gameObject.GetComponent<CoverShooter.MouseLock>().enabled = true;
+    }
+}
