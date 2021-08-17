33dfd3c16f068b1642bc2edefb7b1b2901039c72
GlaM4X <88687626+GlaM4X@users.noreply.github.com>
2021-08-17 16:53:16 +0300

    Zero Fix

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -8,7 +8,22 @@ public class PlayerScript : NetworkBehaviour
 {
     void Start()
     {
-        
+        //Если не локальный Player
+        if (!isLocalPlayer)
+        {
+            //Удаляем компоненты управления персонажем
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterMotor>());
+            Destroy(gameObject.GetComponent<CoverShooter.ExitToEscape>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterPlatform>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterFace>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterAlerts>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterInventory>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterEffects>());
+            Destroy(gameObject.GetComponent<CoverShooter.ThirdPersonInput>());
+            Destroy(gameObject.GetComponent<CoverShooter.ThirdPersonController>());
+            Destroy(gameObject.GetComponent<CoverShooter.Actor>());
+            Destroy(gameObject.GetComponent<CoverShooter.CharacterSounds>());
+        }
     }
 
     void Update()
@@ -18,8 +33,9 @@ public class PlayerScript : NetworkBehaviour
 
     public override void OnStartLocalPlayer()
     {
+        //Находим цель для камеры
         Camera.main.gameObject.GetComponent<CoverShooter.ThirdPersonCamera>().Target = gameObject.GetComponent<CoverShooter.CharacterMotor>();
-
+        //Прячем мышь
         Camera.main.gameObject.GetComponent<CoverShooter.MouseLock>().enabled = true;
     }
 }
